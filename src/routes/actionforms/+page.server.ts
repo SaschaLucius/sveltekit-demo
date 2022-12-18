e49a0e20fb09da1765a3fe69a6ebf0aa9dc06c76
Sascha Lucius <sascha.lucius@datameer.com>
2022-12-18 17:14:41 +0100

    Update +page.server.ts

--- src/routes/actionforms/+page.server.ts
+++ src/routes/actionforms/+page.server.ts
@@ -1,5 +1,8 @@
 import type { Actions } from './$types';
 
+// do not prerender this page, because it needs form data
+export const prerender = false;
+
 export const actions: Actions = {
 	// default action
 	default: async (event) => {
