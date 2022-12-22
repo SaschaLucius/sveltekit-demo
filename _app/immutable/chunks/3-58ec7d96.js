d3d201f87338b13f9b355111daf7b01a625d6f92
SaschaLucius <SaschaLucius@users.noreply.github.com>
2022-12-22 08:28:33 +0000

    deploy: 075cf9f422b270a9ff735edb1044ec39b1386beb

--- _app/immutable/chunks/3-58ec7d96.js
+++ _app/immutable/chunks/3-58ec7d96.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-6a6e5f22.js";import{default as t}from"../components/pages/about/_page.svelte-cc6b2917.js";export{t as component,r as shared};
+import{_ as r}from"./_page-9c76c2d7.js";import{default as t}from"../components/pages/about/_page.svelte-cc6b2917.js";export{t as component,r as shared};
